0aab8af3a7740585841843fb844e9859a8675b6c
MRongM <1399316847@qq.com>
2025-07-17 14:49:30 +0800

    update

--- src/PageData.js
+++ src/PageData.js
@@ -1,6 +1,7 @@
 const pages = [
-    { title: '配置文件生成', description: 'Make Coding Great Again', url: 'https://generator.328064.xyz' , iconUir: '/app-icons/configure-generator-icon.png'},
-    { title: 'Shell常用命令', description: 'Make Shell Great Again', url: 'https://gh.328064.xyz/md/shell' , iconUir: '/app-icons/one-line-code-icon.png'},
-    { title: 'Python常用命令', description: 'Make Python Great Again', url: 'https://gh.328064.xyz/md/python' , iconUir: '/app-icons/one-line-code2-icon.png'},
+    { title: '随机密码生成', description: 'Make Password Great Again', url: 'https://password.328064.xyz/' , iconUir: '/app-icons/password-icon.png'},
+    { title: '配置文件生成', description: 'Make Project Great Again', url: 'https://generator.328064.xyz' , iconUir: '/app-icons/configure-generator-icon.png'},
+    { title: 'Shell瑞士军刀', description: 'Make Shell Great Again', url: 'https://gh.328064.xyz/md/shell' , iconUir: '/app-icons/one-line-code-icon.png'},
+    { title: 'Python瑞士军刀', description: 'Make Python Great Again', url: 'https://gh.328064.xyz/md/python' , iconUir: '/app-icons/one-line-code2-icon.png'},
 ];
 export default pages;
