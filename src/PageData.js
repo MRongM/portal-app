96bea5b4f525426a184f19445da8a6801e1ce396
MRongM <1399316847@qq.com>
2025-08-04 09:59:56 +0800

    update

--- src/PageData.js
+++ src/PageData.js
@@ -1,4 +1,5 @@
 const pages = [
+    { title: 'UUID生成', description: 'Make UUID Great Again', url: 'https://uuid.328064.xyz/' , iconUir: '/app-icons/uuid-icon.png'},
     { title: '随机密码生成', description: 'Make Password Great Again', url: 'https://password.328064.xyz/' , iconUir: '/app-icons/password-icon.png'},
     { title: '配置文件生成', description: 'Make Project Great Again', url: 'https://generator.328064.xyz/' , iconUir: '/app-icons/configure-generator-icon.png'},
     { title: 'Shell瑞士军刀', description: 'Make Shell Great Again', url: 'https://shell.328064.xyz/' , iconUir: '/app-icons/one-line-code-icon.png'},
